6258f05bf243fc8afdbaf051685e535296a463b1
iB Arif <ibhacker2018@gmail.com>
2022-01-25 19:56:34 +0600

    initial file added

---
+++ problem3.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main (){
+
+        int i , sum = 0 , n ;
+
+        printf("Enter any desimal Number : ");
+
+        scanf("%d", &n);
+
+        printf("The first %d natural number is :\n", n);
+
+        for(i = 1; i <= n; i++)
+        {
+            sum = sum + i ;
+        printf("%d ",i);
+        } 
+        printf("\nThe Sum is : %d\n", sum);
+    return 0;
+}
