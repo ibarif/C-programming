c9aaa3ed9df9ae00d2cfcdf3aedba6961c64138b
ARIF HOSSEN <ibhacker2018@gmail.com>
2022-02-07 19:00:05 +0600

    The Sum of odd Natural Number

---
+++ OddAndSum.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+    int limit, sum = 0, counter = 0;
+
+    printf("Input number of terms : ");
+    scanf("%d", &limit);
+    printf("The odd numbers are : ");
+ 
+    for (int index = 1; counter < limit; index++)
+    {
+            if (index%2 == 1)
+            {
+                printf("%d ",index);
+                counter++;
+                sum = sum + index; 
+            }
+                    
+      }
+    printf("\nThe Sum of odd Natural Number upto 10 terms : %d\n", sum);
+
+    return 0;
+}
