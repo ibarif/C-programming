d2e99d6f636f4334c1aa27ba6aad5ae546f8a96d
ARIF HOSSEN <ibhacker2018@gmail.com>
2022-02-19 21:33:06 +0600

    Work par hour salary

---
+++ workHours.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main (){
+    int Ca, th, Lh, Gh, Salry;
+            
+    do{
+        printf("Enter your card number : ");
+        scanf("%d", &Ca);
+
+        printf("Enter your Total work hours : ");
+        scanf("%d", &th);
+        getchar();
+
+        Lh = th*10;
+        Gh = 40*10 + ( th-40 ) *20;
+            if(th > 40)
+            {
+            printf("\nCard number %d , Total Hours %d , Total salry %d\n\n", Ca, th, Gh);
+            }else{
+                printf("\nCard number %d , Total Hours %d , Total salry %d\n\n", Ca, th, Lh);
+            }
+        printf("if you try again prass enter , else enter any key\n\n");
+        
+    } while(getchar() == '\n');
+    
+return 0;
+
+}
