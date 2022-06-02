515b6a34d0e190a6f40f6dba987d8da82521c7dc
ARIF HOSSEN <ibhacker2018@gmail.com>
2022-06-02 14:47:16 +0600

    Area of square

---
+++ square.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+// Area of square .
+
+int main () {
+
+    float side;
+
+    printf("Enter side : ");
+    scanf("%f", &side);
+
+    printf("Area is %.2f\n", side * side);
+
+return 0;
+
+}
