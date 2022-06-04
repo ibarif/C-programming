7e33e8d6aaaabdfcaabc4b6aa37520bc08402afc
ARIF HOSSEN <ibhacker2018@gmail.com>
2022-06-04 16:37:13 +0600

    Arithmetic Instructions

---
+++ arithmetic.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+//Arithmetic Instructions
+
+int main() {
+
+    int a = 1, b = 2, c = 3;
+
+    //valid
+    a = b + c;
+
+    //invalid
+    // b + c = a;
+
+    printf("%d \n", 3 % 2);
+    printf("%d \n", -3 % 2);
+
+return 0;
+
+}
